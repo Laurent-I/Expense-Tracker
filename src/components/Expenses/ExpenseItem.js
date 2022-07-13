47a8f88008eeb325c89de6ffef51ec74162f3e87
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-07-13 10:47:05 +0200

    The expense tracker with react.js

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,24 @@
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
+import './ExpenseItem.css';
+import React, {useState} from 'react';
+
+const ExpenseItem = (props) =>{
+  const [title, setTitle]= useState(props.title);
+  const clickHandler = () =>{
+    setTitle('Laurent'); 
+    console.log(title);
+  }
+  return (
+    <Card className='expense-item'>
+      <ExpenseDate date={props.date} />
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
+      </div>
+      {/*<button onClick={clickHandler}>Change title</button>*/}
+    </Card>
+  );
+}
+
+export default ExpenseItem;
---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,41 @@
+import React,{useState} from "react";
+import ExpenseItem from './ExpenseItem';
+import Card from '../UI/Card';
+import './Expenses.css';
+import ExpensesFilter from './ExpenseFilter'
+
+const Expenses =(props) => {
+        const [filteredYear, setFilteredYear] = useState('2020');
+    const filterChangeHandler = selectedYear => {
+        setFilteredYear(selectedYear);
+    }
+
+    const filteredExpenses = props.items.filter(expense => {
+        return expense.date.getFullYear().toString() === filteredYear;
+    });
+
+    let expensesContent =<p>No Expenses Found.</p>;
+
+    if(filteredExpenses.length > 0) {
+        expensesContent =filteredExpenses.map((expense) => (
+            <ExpenseItem
+                key={expense.id}
+                title={expense.title}
+                amount={expense.amount}
+                date={expense.date}
+            />
+        ))
+    }
+
+  return (
+    <Card className="expenses">
+        <ExpensesFilter
+            selected={filteredYear}
+            onChangeFilter={filterChangeHandler}
+        />
+        {expensesContent}
+    </Card>
+  );
+}
+
+export default Expenses;
